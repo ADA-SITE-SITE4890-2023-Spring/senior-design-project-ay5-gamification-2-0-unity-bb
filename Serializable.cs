7cca9d80a68138a7dc021b30f1a9c3d0778f4cca
fatimakhalifali <58476110+fatimakhalifali@users.noreply.github.com>
2023-02-04 20:26:14 +0400

    Add files via upload

---
+++ Serializable.cs
@@ -0,0 +1,40 @@
+﻿[System.Serializable]
+
+public class Course
+{
+	public string id;
+	public string courseId;
+	public string name;
+	public bool organization;
+	public string ultraStatus;
+	public bool readOnly;
+	public Availability1 availability;
+	public Enrollment1 enrollment;
+	public Locale1 locale;
+	public string externalAccessUrl;
+}
+
+[System.Serializable]
+public class Availability1
+{
+	public string available;
+	public Duration1 duration;
+}
+
+[System.Serializable]
+public class Duration1
+{
+	public string type;
+}
+
+[System.Serializable]
+public class Locale1
+{
+	public bool force;
+}
+
+[System.Serializable]
+public class Enrollment1
+{
+	public string type;
+}
