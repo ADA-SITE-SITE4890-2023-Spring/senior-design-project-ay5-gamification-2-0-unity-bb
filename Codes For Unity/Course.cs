71e77cc9cfbcf3513e7f273ba948f4c63c51eb95
Tangiz Alizada <77734140+Tenoo@users.noreply.github.com>
2023-02-04 22:43:28 +0400

    Update Course.cs

--- Codes For Unity/Course.cs
+++ Codes For Unity/Course.cs
@@ -1,6 +1,39 @@
 [System.Serializable]
 
-public class Course
-{
-	public string name;
-}
+
+    public class Course
+    {
+      public string id;
+      public string courseId;
+      public string name;
+      public string title;
+      public bool organization;
+      public string ultraStatus;
+      public bool readOnly;
+      public Availability availability;
+      public Enrollment enrollment;
+      public Locale locale;
+      public string externalAccessUrl;
+    }
+
+
+    public class Availability
+    {
+      public string available;
+      public Duration duration;
+    }
+
+    public class Duration
+    {
+      public string type;
+    }
+
+    public class Enrollment
+    {
+      public string type;
+    }
+
+    public class Locale
+    {
+      public bool force;
+    }
