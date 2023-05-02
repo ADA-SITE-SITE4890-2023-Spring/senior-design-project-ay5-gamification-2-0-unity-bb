2220dbae96b304b75bbde78084c9f60845cf91e7
Tangiz Alizada <77734140+Tenoo@users.noreply.github.com>
2023-05-03 00:43:40 +0400

    Update Lectures.cs

--- finalversion/Assets/Scripts/Lectures.cs
+++ finalversion/Assets/Scripts/Lectures.cs
@@ -20,7 +20,7 @@ public class Lectures : MonoBehaviour, IPointerClickHandler
             if (transform.parent.GetChild(0).GetComponentInChildren<TextMeshProUGUI>().text + lct.title == transform.GetComponentInChildren<TextMeshProUGUI>().text)
             {
                 File lecture = await APIManager.Instance.GetContentAttachmentsAsync(PlayerController.Instance.currentRoom, lct.id, GameManager.Instance.authContext.access_token);
-                APIManager.Instance.DownloadAttachmentAsync(PlayerController.Instance.currentRoom, lct.id, lecture.id, GameManager.Instance.authContext.access_token);
+                APIManager.Instance.DownloadAttachmentAsync(PlayerController.Instance.currentRoom, lct.id, lecture.id, GameManager.Instance.authContext.access_token, "lecture" + lct.id);
             }
         }
     }
